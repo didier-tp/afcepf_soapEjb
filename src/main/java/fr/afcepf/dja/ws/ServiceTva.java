a93e8ed888ae22314c75d38ae16a1aa4f3acee05
didier <didier-defrance@wanadoo.fr>
2017-11-16 16:37:04 +0100

    aa

--- src/main/java/fr/afcepf/dja/ws/ServiceTva.java
+++ src/main/java/fr/afcepf/dja/ws/ServiceTva.java
@@ -9,7 +9,7 @@ import fr.afcepf.dja.data.ResCalculTva;
 @WebService()//avec namespace par defaut : http:// + package java à l'envers + /
              //http://ws.dja.afcepf.fr/
 //@SOAPBinding(style = SOAPBinding.Style.DOCUMENT) par defaut en java , en .net, ...
-@Local
+//@Local
 public interface ServiceTva {
 	
 	public double tva(@WebParam(name="ht")double ht,
--- src/main/java/fr/afcepf/dja/ws/ServiceTvaImpl.java
+++ src/main/java/fr/afcepf/dja/ws/ServiceTvaImpl.java
@@ -7,7 +7,7 @@ import javax.jws.WebService;
 import fr.afcepf.dja.data.ResCalculTva;
 
 @Stateless
-//@Local //ou bien @Remote (NB: @Local ou @Remote sur interface ou bien sur classe) 
+@Local //ou bien @Remote (NB: @Local ou @Remote sur interface ou bien sur classe) 
 @WebService(endpointInterface="fr.afcepf.dja.ws.ServiceTva")
 //NB: url du WS soap (et du ?wsdl) affichée dans la console jboss au démarrage
 //pas besoin de Endpoint.publish() , ce sera fait automatiquement par le serveur JEE jboss
