6cdedfcf5c0aa760b2fd54f3af5ac91e843e6bf7
didier <didier-defrance@wanadoo.fr>
2017-11-16 16:29:35 +0100

    aa

--- src/main/java/fr/afcepf/dja/ws/ServiceTva.java
+++ src/main/java/fr/afcepf/dja/ws/ServiceTva.java
@@ -1,14 +1,15 @@
 package fr.afcepf.dja.ws;
 
+import javax.ejb.Local;
 import javax.jws.WebParam;
 import javax.jws.WebService;
-import javax.jws.soap.SOAPBinding;
 
 import fr.afcepf.dja.data.ResCalculTva;
 
 @WebService()//avec namespace par defaut : http:// + package java à l'envers + /
              //http://ws.dja.afcepf.fr/
 //@SOAPBinding(style = SOAPBinding.Style.DOCUMENT) par defaut en java , en .net, ...
+@Local
 public interface ServiceTva {
 	
 	public double tva(@WebParam(name="ht")double ht,
--- src/main/java/fr/afcepf/dja/ws/ServiceTvaImpl.java
+++ src/main/java/fr/afcepf/dja/ws/ServiceTvaImpl.java
@@ -1,17 +1,28 @@
 package fr.afcepf.dja.ws;
 
-import javax.ejb.Remote;
+import javax.ejb.Local;
 import javax.ejb.Stateless;
 import javax.jws.WebService;
 
 import fr.afcepf.dja.data.ResCalculTva;
 
 @Stateless
-@Remote //ou bien @Local (NB: @Local ou @Remote sur interface ou bien sur classe) 
+//@Local //ou bien @Remote (NB: @Local ou @Remote sur interface ou bien sur classe) 
 @WebService(endpointInterface="fr.afcepf.dja.ws.ServiceTva")
 //NB: url du WS soap (et du ?wsdl) affichée dans la console jboss au démarrage
 //pas besoin de Endpoint.publish() , ce sera fait automatiquement par le serveur JEE jboss
 public class ServiceTvaImpl implements ServiceTva {
+	
+	@Override
+	public ResCalculTva calculerTvaEtTtc(double ht, double taux) {
+		ResCalculTva resCalculTva = new ResCalculTva();
+		resCalculTva.setHt(ht);
+		resCalculTva.setTaux(taux);
+		resCalculTva.setTva(ht*taux/100.0);
+		resCalculTva.setTtc(ht + resCalculTva.getTva());
+		System.out.println("sur ejb: resCalculTva="+resCalculTva);
+		return resCalculTva;
+	}
 
 	@Override
 	public double tva(double ht, double taux) {
@@ -23,14 +34,6 @@ public class ServiceTvaImpl implements ServiceTva {
 		return "didier defrance / formateur fou";
 	}
 
-	@Override
-	public ResCalculTva calculerTvaEtTtc(double ht, double taux) {
-		ResCalculTva resCalculTva = new ResCalculTva();
-		resCalculTva.setHt(ht);
-		resCalculTva.setTaux(taux);
-		resCalculTva.setTva(ht*taux/100.0);
-		resCalculTva.setTtc(ht + resCalculTva.getTva());
-		return resCalculTva;
-	}
+	
 
 }
